cc8dc7b483a834d6934f6934f0f9e5d60b324c03
Michael Romero <mromero.working@gmail.com>
2025-03-03 13:07:18 -0600

    Mostrar amigos en una lista (ul->li) - Función mostrarAmigos(array)

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 //Creando el arreglo donde se guardarán los nombre
 let amigos = [];
 
-//Agregando amigo al arreglo
+//Agregando amigo al arreglo y validando que la caja texto no este vacía.
 function agregarAmigo() {
     let texto = document.getElementById("amigo").value.trim();
 
     if (texto != "" && texto != " ") {
         amigos.push(texto);
         limpiarCajaTexto();
+        mostrarAmigos(amigos);
     } else {
         alert("Por favor, inserte un nombre");
     }
@@ -16,4 +17,16 @@ function agregarAmigo() {
 //Limpiando la caja de texto
 function limpiarCajaTexto() {
     document.getElementById("amigo").value = '';
+}
+
+//Mostrar amgigos en una lista (ul->li)
+function mostrarAmigos(arreglo) {
+    document.getElementById("listaAmigos").innerHTML = '';
+
+    for (let i = 0; i < arreglo.length; i++) {
+        listaAmigos = document.getElementById("listaAmigos");
+        let nuevoElemento = document.createElement("li");
+        nuevoElemento.textContent = arreglo[i];
+        listaAmigos.appendChild(nuevoElemento);
+    }
 }
