49c43b0ade6f02c488cdc614c506b5ddf22d6c2b
Michael Romero <mromero.working@gmail.com>
2025-03-03 11:24:01 -0600

    Agregando amigo al arreglo, validando que la caja texto no este vacía y limpiar la caja de texto

--- app.js
+++ app.js
@@ -1,2 +1,19 @@
-//Creando el arreglo donde se guardarán los nombres
+//Creando el arreglo donde se guardarán los nombre
 let amigos = [];
+
+//Agregando amigo al arreglo
+function agregarAmigo() {
+    let texto = document.getElementById("amigo").value.trim();
+
+    if (texto != "" && texto != " ") {
+        amigos.push(texto);
+        limpiarCajaTexto();
+    } else {
+        alert("Por favor, inserte un nombre");
+    }
+}
+
+//Limpiando la caja de texto
+function limpiarCajaTexto() {
+    document.getElementById("amigo").value = '';
+}
