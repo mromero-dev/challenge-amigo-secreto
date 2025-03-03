f2e5734af18aece69b2f734488356041a87c9049
Michael Romero <mromero.working@gmail.com>
2025-03-03 15:57:46 -0600

    Agregando la funcion sortearAmigo y reiniciarSorteo en app.js

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ let amigos = [];
 
 //Agregando amigo al arreglo y validando que la caja texto no este vacía.
 function agregarAmigo() {
-    let texto = document.getElementById("amigo").value.trim();
+    let texto = document.getElementById("amigo");
+    let valorTexto = texto.value.trim()
 
-    if (texto != "" && texto != " ") {
-        amigos.push(texto);
+    if (valorTexto != "" && valorTexto != " ") {
+        amigos.push(valorTexto);
         limpiarCajaTexto();
         mostrarAmigos(amigos);
+        texto.focus();
     } else {
         alert("Por favor, inserte un nombre");
     }
@@ -29,4 +31,28 @@ function mostrarAmigos(arreglo) {
         nuevoElemento.textContent = arreglo[i];
         listaAmigos.appendChild(nuevoElemento);
     }
+}
+
+//Creando la función para sortear un amigo
+function sortearAmigo() {
+    if (amigos.length != 0) {
+        let amigoSorteado = amigos[Math.floor(Math.random() * amigos.length)];
+        document.getElementById("resultado").innerHTML = `El amigo sorteado es: ${amigoSorteado}`;
+    } else {
+        alert('Aún no existen elementos para sortear');
+        document.getElementById("amigo").focus();
+    }
+}
+
+//Reiniciando el Sorteo
+function reinciarSorteo() {
+    if (amigos.length > 0) {
+        amigos.length = 0;
+        document.getElementById("listaAmigos").innerHTML = '';
+        document.getElementById("resultado").innerHTML = '';
+        alert('Se ha reiniciado el sorteo, ingresa nuevos nombres por favor.');
+    } else {
+        alert('Aún no existen amigos que sortear');
+    }
+    document.getElementById("amigo").focus();
 }
